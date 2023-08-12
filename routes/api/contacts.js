e6ec94d21de4c7b94f5c4e8538a54fa9d01ca3a9
Rachel Frederickson <vampirerachel@gmail.com>
2023-08-11 22:04:31 -0400

    extra error message added

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res, next) => {
   }
   catch (error) {
     console.error('Error fetching contacts:', error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'Server error', message: error.message });
   }
 });
 
